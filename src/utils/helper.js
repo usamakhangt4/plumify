becec1da1d8c3add108b23c4540863701e08a0c3
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-05-15 20:34:13 +0500

    add: add helper function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,1 +1,8 @@
 export const isEmptyObject = (obj) => Object.keys(obj).length === 0;
+
+
+export function secondsToMinutes(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
