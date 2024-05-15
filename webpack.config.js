931743523ecd07fd1cecee3a37756e71350b86e5
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-05-15 20:34:00 +0500

    fix: fix build issue

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     devtool: false,
     resolve: {
         alias: {
-            'vue': 'node_modules/vue/dist/vue.esm-bundler.js'
+            'vue$': path.resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js')
         }
     },
     module: {
